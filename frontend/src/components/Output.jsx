1a13bd49050254ed368b6e40deb98ed6053ef54c
Tom-the-Bomb <thello522@gmail.com>
2023-06-07 23:15:13 -0400

    Latex!

--- frontend/src/components/Output.jsx
+++ frontend/src/components/Output.jsx
@@ -1,9 +1,23 @@
-function Section({name, content}) {
+import { useRef, useEffect } from "react";
+
+var katex = require('katex');
+
+function Latex({content}) {
+    const container = useRef();
+
+    useEffect(() => {
+        katex.render(content, container.current);
+    }, [content]);
+
+    return <div ref={container} />
+}
+
+function Section({latex, name, content}) {
     return (
         <div className="flex flex-col gap-4">
             <h1 className="my-h1">{name}</h1>
             <div className="font-mono p-4 rounded-md bg-rose-800 text-rose-300">
-                {content}
+                {latex ? <Latex content={content}></Latex> : content}
             </div>
         </div>
     )
@@ -20,12 +34,14 @@ export default function Output({response}) {
         }
         return (
             <div className="mt-10">
-                <Section name="Simplified" content={response.content.simplified_equation}></Section>
-                <Section name="Solution" content={response.content.latex_solution}></Section>
-                <Section name="Derivative" content={response.content.derivative}></Section>
+                <Section latex={true} name="Simplified" content={response.content.simplified_equation}></Section>
+                <Section latex={true} name="Solution" content={response.content.latex_solution}></Section>
+                <Section latex={true} name="Derivative" content={response.content.derivative}></Section>
             </div>
         )
     } else {
-        return ""
+        return (
+            <p className="text-rose-300 text-2xl mt-10 bold">Enter some math first...</p>
+        )
     }
 }
