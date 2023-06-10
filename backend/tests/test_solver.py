cda344ef8482a35d3cf46ca47768c0d83ee2601b
Tom-the-Bomb <thello522@gmail.com>
2023-06-09 22:58:04 -0400

    fix tests

--- backend/tests/test_solver.py
+++ backend/tests/test_solver.py
@@ -9,7 +9,7 @@ __all__ = (
 
 def test_parsing() -> None:
     equations = (
-        "x^-1"
+        "2 + 3 * 6root2x -  4 * 5",
         "root(2x + 1)2x+1",
         "[2y]2sin(2[x])",
         "1 - 2 + -x + -3 - 4 + 5x - +0! != 7 % 2",
